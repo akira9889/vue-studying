cece045ca24cfe46343bb6ac3651fc6f5caf1cef
akira9889 <aki_badmin89@icloud.com>
2023-04-22 16:04:21 +0900

    404ページ追加

---
+++ section7/vuerouter/src/components/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    404 ページが存在しません。<br>
+    <button @click="goToHome">ホームに戻る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFound',
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+    goToHome() {
+      this.$router.push('/')
+    }
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
