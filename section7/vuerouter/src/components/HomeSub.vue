a1c189b055436591a191c001c06fad66ca329e50
akira9889 <aki_badmin89@icloud.com>
2023-04-22 16:10:32 +0900

    名前付きビュー

---
+++ section7/vuerouter/src/components/HomeSub.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    サブのコンポーネントになります
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
