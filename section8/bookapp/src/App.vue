6220f26fe23e6547824e012db20531510f22881b
akira9889 <aki_badmin89@icloud.com>
2023-04-27 18:24:23 +0900

    CRUD操作追加

--- section8/bookapp/src/App.vue
+++ section8/bookapp/src/App.vue
@@ -2,7 +2,13 @@
   <v-app>
     <HeaderView />
     <v-main>
-      <router-view />
+      <v-container>
+        <router-view
+        :books="books"
+        @add-book-list="addBook"
+        @update-book-info="updateBookInfo"
+        @delete-book="deleteBook"/>
+      </v-container>
     </v-main>
     <FooterView />
   </v-app>
@@ -11,6 +17,7 @@
 <script>
 import HeaderView from '@/global/HeaderView.vue';
 import FooterView from '@/global/FooterView.vue';
+const STORAGE_KEY = 'books'
 
 export default {
   name: 'App',
@@ -18,8 +25,65 @@ export default {
     HeaderView,
     FooterView
   },
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      books: [],
+      nextId: 1
+    }
+  },
+  methods: {
+    addBook(e) {
+      this.books.push({
+        id: this.nextId,
+        title: e.title,
+        image: e.image,
+        description: e.description,
+        readDate: '',
+        memo: ''
+      })
+      this.nextId++
+      this.saveBooks()
+      //最後に追加したidの取得コード
+      this.goToEditPage(this.books.slice(-1)[0].id)
+    },
+    updateBookInfo(e) {
+      let index = e.id -1
+      const updateInfo = {
+        id: e.id,
+        readDate: e.readDate,
+        memo: e.memo,
+        title: this.books[index].title,
+        image: this.books[index].image,
+        description: this.books[index].description
+      }
+
+      this.books.splice(index, 1, updateInfo)
+      this.saveBooks()
+      this.$router.push('/')
+    },
+    deleteBook(id) {
+      let index = this.books.findIndex(book => book.id === id)
+      this.books.splice(index, 1)
+      this.saveBooks()
+    },
+    saveBooks() {
+      const parsed = JSON.stringify(this.books)
+      localStorage.setItem(STORAGE_KEY, parsed)
+    },
+    goToEditPage(id) {
+      this.$router.push(`/edit/${id}`)
+    }
+  },
+    mounted() {
+    if (localStorage.getItem(STORAGE_KEY)) {
+      try {
+        this.books = JSON.parse(localStorage.getItem(STORAGE_KEY))
+        let ids = this.books.map(book => book.id)
+        this.nextId = Math.max(...ids) + 1
+      } catch {
+        localStorage.removeItem(STORAGE_KEY)
+      }
+    }
+  },
 };
 </script>
