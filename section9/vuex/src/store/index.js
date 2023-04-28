5a246038cfcad643eef005675a5a0e272c598fec
akira9889 <aki_badmin89@icloud.com>
2023-04-28 12:29:56 +0900

    vuex学習

---
+++ section9/vuex/src/store/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    count: 0,
+    users: [
+      {id: 1, name: '大谷', isVisible: true},
+      {id: 2, name: 'ダルビッシュ', isVisible: true},
+      {id: 3, name: '錦織圭', isVisible: true}
+    ]
+  },
+  getters: {
+    // visibleUsers(state) {
+    //   return state.users.filter(user => user.isVisible)
+    // }
+    visibleUsers: state => state.users.filter(user => user.isVisible),
+
+    getUserById: state => id => {
+      return state.users.find(user => user.id === id)
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++
+    },
+    addCount(state, payload) {
+      state.count += payload.value
+    }
+  },
+  actions: {
+    // incrementAction(context) {
+    //   context.commit('increment')
+    // },
+    incrementAction({commit}) {
+      commit('increment')
+    },
+    addCountAction({ commit }, payload) {
+      commit('addCount', payload)
+    }
+  },
+  modules: {
+  }
+})
