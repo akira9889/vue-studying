34237cd98d7e5360ee02a9da6a75440855fa7465
akira9889 <aki_badmin89@icloud.com>
2023-04-28 17:14:10 +0900

    provide,inject

---
+++ section10/vue3-test/src/components/GrandChildren.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    GrandChildren
+  </div>
+  <div>{{ userName }}</div>
+</template>
+
+<script>
+export default{
+  name: 'GrandChildren',
+  data() {
+    return {
+    }
+  },
+  inject: ['userName']
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ section10/vue3-test/src/views/ChildrenView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    Children
+    <grand-children />
+  </div>
+</template>
+
+<script>
+import GrandChildren from "@/components/GrandChildren.vue";
+export default {
+  name: 'ChildrenView',
+  components: {
+    GrandChildren
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  mounted() {
+
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
