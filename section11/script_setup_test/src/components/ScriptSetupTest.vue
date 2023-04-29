7ab568c9862a0dd9b3032b7343a28b3364012998
akira9889 <aki_badmin89@icloud.com>
2023-04-29 16:03:47 +0900

    script setup 学習

---
+++ section11/script_setup_test/src/components/ScriptSetupTest.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  title: String
+})
+
+const emitTest = defineEmits(['custom-event'])
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
+
+const decrement = () => {
+  count.value--
+}
+</script>
+<template>
+  <h1>{{ count }}</h1>
+  <p>{{ props.title }}</p>
+  <button @click="emitTest('custom-event', '子からの値')">エミット</button>
+  <button @click="increment">Increment</button>
+  <button @click="decrement">Decrement</button>
+</template>
+
+
+<style scoped>
+</style>
